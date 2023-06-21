f4a19738bb3ed37fd2b0290c2e6921ec6384a183
mkireva <mariya@kireva.de>
2023-06-21 15:20:30 +0200

    add: auth

---
+++ apps/auth/src/strategies/jwt-strategy.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { Injectable } from '@nestjs/common';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly userService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: any) =>
+          request?.cookies?.Authentication ||
+          request?.Authentication ||
+          request?.headers.Authentication,
+      ]),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+  async validate({ userId }: TokenPayload) {
+    return this.userService.getUser({ _id: userId });
+  }
+}
